bb9c5095041af5bfcab465edcad3036e081260a1
Theik Chan <theik.chan@wavemoney.com.mm>
2024-09-12 00:39:19 +0700

    Initial Commit

---
+++ SwiftUIComponent/SwiftUIComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentApp.swift
+//  SwiftUIComponent
+//
+//  Created by Theik Chan on 12/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
