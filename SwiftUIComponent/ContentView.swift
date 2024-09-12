4fb7ef8be1e15ebc8bf021bd0057ca851ce0e4ac
Theik Chan <theikchan.tc@gmail.com>
2024-09-12 11:53:39 +0700

    feat: text and textfield component

--- SwiftUIComponent/ContentView.swift
+++ SwiftUIComponent/ContentView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var name: String = ""
+    
     var body: some View {
         VStack {
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            let markdownString = try! AttributedString(markdown: "Sign up to our Newsletter and get **30%** off!")
+            
+            Text(markdownString)
+                .font(.system(size: 24,weight: .light, design: .serif))
+                .italic().lineLimit(1)
+            
+            
+            TextField("Enter your name", text: $name,
+                      onEditingChanged: { status in
+                    print(status)
+                })
+                .onChange(of: name, { oldValue, newValue in
+                    print(newValue)
+                })
+                .onSubmit {
+                    print("submitted")
+                }
+                .padding()
+            
         }
         .padding()
     }
